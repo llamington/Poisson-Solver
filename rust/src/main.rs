3c9c08825082875f6766886aef42ac8ef3082cd2
Bailey <lissington4@gmail.com>
2022-11-09 09:01:43 +1300

    working result

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,10 +1,10 @@
-use std::process::ExitCode;
-
 use clap::Parser;
-
+use std::process::ExitCode;
+use util::tensor_idx;
 mod poisson_solver;
-// use poisson_solver::PoissonSolver;
 mod util;
+
+const DELTA: f32 = 1.0;
 
 /// Program for solving a 3D discrete form of the Poisson Equation
 #[derive(Parser, Debug)]
@@ -19,7 +19,7 @@ struct Args {
 
     /// Number of threads
     #[arg(short, long)]
-    threads: Option<u8>,
+    threads: Option<usize>,
 }
 
 fn main() -> ExitCode {
@@ -33,8 +33,25 @@ fn main() -> ExitCode {
         eprintln!("Expected an odd size");
         return ExitCode::FAILURE;
     }
+    let threads = match threads {
+        None => std::thread::available_parallelism()
+            .expect("Parallelism data unavailable. Please manually input a thread count")
+            .get(),
+        Some(t) => t,
+    };
     let mut source: Vec<f32> = vec![0.0; size * size * size];
     source[util::tensor_idx!(size / 2, size / 2, size / 2, size)] = 1.0;
-    let poisson_solver = poisson_solver::PoissonSolver::new(size, &source, iterations, threads, delta)
+    let mut poisson_solver =
+        poisson_solver::PoissonSolver::new(size, &source, iterations, threads, DELTA);
+    let solution = poisson_solver.solve();
+
+    // Print the central slice of the solution
+    for j in 0..size {
+        for k in 0..size {
+            print!("{:.5} ", solution[tensor_idx!(size / 2, j, k, size)])
+        }
+        println!();
+    }
+
     return ExitCode::SUCCESS;
 }
--- rust/src/poisson_solver.rs
+++ rust/src/poisson_solver.rs
@@ -1,11 +1,13 @@
+use crate::util::tensor_idx;
+
 pub struct PoissonSolver<'a> {
     size: usize,
     iterations: u16,
     source: &'a Vec<f32>,
-    threads: u8,
+    threads: usize,
     delta: f32,
-    curr: *mut Vec<f32>,
-    next: *mut Vec<f32>,
+    curr: Box<Vec<f32>>,
+    next: Box<Vec<f32>>,
 }
 
 impl<'a> PoissonSolver<'a> {
@@ -13,7 +15,7 @@ impl<'a> PoissonSolver<'a> {
         size: usize,
         source: &'a Vec<f32>,
         iterations: u16,
-        threads: u8,
+        threads: usize,
         delta: f32,
     ) -> Self {
         PoissonSolver {
@@ -22,9 +24,52 @@ impl<'a> PoissonSolver<'a> {
             source,
             threads,
             delta,
-            curr: &mut vec![0.0; size * size * size],
-            next: &mut vec![0.0; size * size * size],
+            curr: Box::new(vec![0.0; size * size * size]),
+            next: Box::new(vec![0.0; size * size * size]),
         }
     }
-    pub fn solve() -> *mut Vec<f32> {}
+    pub fn solve(&mut self) -> &Box<Vec<f32>> {
+        for _iter in 0..self.iterations {
+            for i in 0..self.size {
+                for j in 0..self.size {
+                    for k in 0..self.size {
+                        let mut v: f32 = 0.0;
+
+                        if i == 0 {
+                            v += 2.0 * (*self.curr)[tensor_idx!(1, j, k, self.size)];
+                        } else if i == self.size - 1 {
+                            v += 2.0 * (*self.curr)[tensor_idx!(self.size - 2, j, k, self.size)];
+                        } else {
+                            v += (*self.curr)[tensor_idx!(i - 1, j, k, self.size)];
+                            v += (*self.curr)[tensor_idx!(i + 1, j, k, self.size)];
+                        }
+
+                        if j == 0 {
+                            v += 2.0 * (*self.curr)[tensor_idx!(i, 1, k, self.size)];
+                        } else if j == self.size - 1 {
+                            v += 2.0 * (*self.curr)[tensor_idx!(i, self.size - 2, k, self.size)];
+                        } else {
+                            v += (*self.curr)[tensor_idx!(i, j - 1, k, self.size)];
+                            v += (*self.curr)[tensor_idx!(i, j + 1, k, self.size)];
+                        }
+
+                        if k == 0 {
+                            v += 2.0 * (*self.curr)[tensor_idx!(i, j, 1, self.size)];
+                        } else if k == self.size - 1 {
+                            v += 2.0 * (*self.curr)[tensor_idx!(i, j, self.size - 2, self.size)];
+                        } else {
+                            v += (*self.curr)[tensor_idx!(i, j, k - 1, self.size)];
+                            v += (*self.curr)[tensor_idx!(i, j, k + 1, self.size)];
+                        }
+
+                        v -= self.delta * self.delta * self.source[tensor_idx!(i, j, k, self.size)];
+                        v /= 6.0;
+                        (*self.next)[tensor_idx!(i, j, k, self.size)] = v;
+                    }
+                }
+            }
+            std::mem::swap(&mut self.curr, &mut self.next);
+        }
+        return &self.curr;
+    }
 }
