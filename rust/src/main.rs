84bd32ef3013d4b3b313d39f199ff5ceefd80895
Bailey <lissington4@gmail.com>
2022-11-08 16:10:04 +1300

    created boilerplate rust code

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,22 +1,38 @@
+use std::process::ExitCode;
+
 use clap::Parser;
+
+// mod util;
+mod util;
 
 /// Program for solving a 3D discrete form of the Poisson Equation
 #[derive(Parser, Debug)]
-#[command()]
 struct Args {
     /// Size of one edge of the volume
     #[arg(short = 'n', long = "size", default_value_t = 7)]
-    size: u16,
+    size: usize,
 
     /// Number of iterations
     #[arg(short, long, default_value_t = 300)]
-    interations: u16,
+    iterations: u16,
 
     /// Number of threads
     #[arg(short, long)]
-    threads: u8,
+    threads: Option<u8>,
 }
 
-fn main() {
-    let args = Args::parse();
+fn main() -> ExitCode {
+    let Args {
+        size,
+        iterations,
+        threads,
+    } = Args::parse();
+
+    if size % 2 == 0 {
+        eprintln!("Expected an odd size");
+        return ExitCode::FAILURE;
+    }
+    let mut source: Vec<f32> = vec![0.0; size * size * size];
+    source[util::tensor_idx!(size / 2, size / 2, size / 2, size)] = 1.0;
+    return ExitCode::SUCCESS;
 }
---
+++ rust/src/util.rs
@@ -0,0 +1,6 @@
+macro_rules! tensor_idx {
+    ($i:expr, $j:expr, $k:expr, $n:expr) => {{
+        $n * (($n * $i + $j) + $k)
+    }};
+}
+pub(crate) use tensor_idx;
